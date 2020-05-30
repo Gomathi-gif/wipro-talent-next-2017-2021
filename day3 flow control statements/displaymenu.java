b7314cb3670f16e517a77d41b24f7452da372c2b
Gomathi-gif <61314863+Gomathi-gif@users.noreply.github.com>
2020-05-30 09:24:31 +0530

    Add files via upload

---
+++ day3 flow control statements/displaymenu.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Main{
+  
+  public static void main(String[] args){
+  
+      Scanner s = new Scanner(System.in);
+     
+   do{
+       
+     System.out.println("1.Add");
+    
+         System.out.println("2.sub");
+     
+        System.out.println("Enter the option");
+   
+          int op = s.nextInt();
+          
+   int i = s.nextInt();
+          
+   int n= s.nextInt();
+    
+         switch(op){
+            
+     case 1:System.out.println(i+n);
+     
+                   break;
+             
+    case 2:System.out.println(i-n);
+    
+	break;
+         }
+        
+     System.out.println("to continue enter y or Y");
+         
+    char yes = s.next.charAt(0);
+       
+      
+             }while(yes=='y' || yes=='Y');
+  
+  }
+}
