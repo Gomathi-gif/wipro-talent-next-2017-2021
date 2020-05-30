f6c8304f87744792044ae5590c04fa1f30ca1f07
Gomathi-gif <61314863+Gomathi-gif@users.noreply.github.com>
2020-05-30 09:20:49 +0530

    Add files via upload

---
+++ class and objects-day3/calc.java
@@ -0,0 +1,26 @@
+import java.lang.Math;
+
+public class Calculator{
+   
+ static double powerInt(int n1, int n2){
+  
+  return Math.pow(n1,n2);
+ 
+   }
+   
+ static double powerDouble(double n1, double n2){
+  
+  return Math.pow(n1,n2);
+   
+ }
+   
+ public static void main(String[] args){
+ 
+       System.out.println(Calculator.powerInt(4,3));
+  
+      System.out.println(Calculator.powerDouble(4.0,4.0));
+ 
+   }
+    
+    
+}
---
+++ class and objects-day3/patient.java
@@ -0,0 +1,39 @@
+class Patient
+{
+  
+  String name;
+  
+  double w,h;
+ 
+   Patient(double weight,double height)
+
+    {
+        w=weight;
+   
+     h=height;
+      
+  
+    }
+  
+  double bmi()
+   
+ {
+        
+double b= (w/(h*h))*703;
+      
+  return b;
+   
+ }
+}
+
+public class Patients{
+
+
+     public static void main(String[] args){
+   
+      Patient p=new Patient(25,32);
+     
+    System.out.println(" BMI of patient"+p.bmi());
+  
+   }
+}
