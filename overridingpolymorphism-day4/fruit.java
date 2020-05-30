782b09966e32e2f33f1b4dccc5c51744f3d67743
Gomathi-gif <61314863+Gomathi-gif@users.noreply.github.com>
2020-05-30 18:32:45 +0530

    Add files via upload

---
+++ overridingpolymorphism-day4/fruit.java
@@ -0,0 +1,67 @@
+import java.util.Scanner;
+
+class Fruit{
+  
+  private String name;
+  
+  private String taste;
+  
+  private int size;
+  
+  Scanner s = new Scanner(System.in);
+ 
+   public void eat(){
+    
+    name=s.next();
+    
+    System.out.prinln("Name of fruit is "+name);
+  
+      taste = s.next();
+  
+      System.out.println("Taste is "+taste);
+ 
+   }
+}
+
+class Apple extends Fruit{
+  
+  public void eat(){
+    
+    System.out.prinln("Name of fruit is apple");
+    
+    System.out.prinln("taste is sweet");
+ 
+   System.out.println("it is heart in shape"); 
+      
+    }
+}
+
+class Orange extends Fruit{
+   
+ public void eat(){
+  
+      System.out.prinln("Name of fruit is orange");
+   
+     System.out.prinln("taste is sour");
+ 
+    System.out.println("it is round ");       
+
+    }
+}
+
+public class Fruits{
+ 
+   public static void main(String[] args){
+ 
+       Apple a=new Apple();
+  
+      a.eat();
+    
+    Orange o=new Orange();
+
+        o.eat;
+      
+  
+    }
+}
+    
---
+++ overridingpolymorphism-day4/shape.java
@@ -0,0 +1,91 @@
+shape c=new Circle();
+
+shape t=new Triangle();
+
+shape s=new Square();
+
+Class Shape{
+  
+  void draw(){
+   
+     System.out.println("drawing");
+   
+ }
+   
+ void erase(){
+ 
+       System.out.println("Erasing");
+ 
+   }
+    
+}
+
+class Circle extends Shape{
+  
+  void draw(){
+    
+    System.out.println("Drawing Circle");
+  
+  }
+  
+  void erase(){
+  
+      System.out.println("Erasing circle");
+ 
+   }
+}
+
+class Triangle extends Shape{
+
+    void draw(){
+  
+      System.out.println("Drawing triangle");
+
+    }
+  
+  void erase(){
+  
+      System.out.println("Erasing triangle");
+  
+  }
+}
+
+class Square extends Shape{
+ 
+   void draw(){
+   
+     System.out.println("Drawing square");
+
+    }
+    
+void erase(){
+   
+     System.out.println("Erasing square");
+  
+  }
+}
+
+public class Polymorphism{
+
+    public static void main(String[] args){
+   
+     shape c=new Circle();
+    
+    shape t=new Triangle();
+      
+   shape s=new Square();
+   
+      c.draw();
+     
+    c.erase();
+   
+       t.draw();
+  
+       t.erase();
+  
+       s.draw();
+         
+s.erase();
+ 
+   }
+}
