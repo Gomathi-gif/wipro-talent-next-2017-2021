a3e69a5879b48766e64b8847358cd9ad861f39af
Gomathi-gif <61314863+Gomathi-gif@users.noreply.github.com>
2020-05-31 09:58:17 +0530

    Add files via upload

---
+++ interface.-day5/library.java
@@ -0,0 +1,35 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// Test case 1
+		KidUsers kidUsers = new KidUsers();
+		
+		kidUsers.setAge(10);
+		kidUsers.setBookType("Kids");
+		kidUsers.registerAccount();
+		kidUsers.requestBook();
+		
+		kidUsers.setAge(18);	
+		kidUsers.setBookType("Fiction");		
+		kidUsers.registerAccount();
+		kidUsers.requestBook();
+
+		System.out.println();
+		
+		// Test case 
+		AdultUser adultUser = new AdultUser();
+		
+		adultUser.setAge(5);
+		adultUser.setBookType("Kids");
+		adultUser.registerAccount();
+		adultUser.requestBook();
+		
+		adultUser.setAge(23);	
+		adultUser.setBookType("Fiction");		
+		adultUser.registerAccount();
+		adultUser.requestBook();
+		
+	}
+
+}
