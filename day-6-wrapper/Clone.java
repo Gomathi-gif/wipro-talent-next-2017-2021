bfba80e0a9a5df01e82b116bd0bcffa4ab97013c
Gomathi-gif <61314863+Gomathi-gif@users.noreply.github.com>
2020-06-03 20:36:44 +0530

    Add files via upload

---
+++ day-6-wrapper/Clone.java
@@ -0,0 +1,39 @@
+public class Main{
+ 
+   public static void  main(String[] args) throws CloneNotSupportedExcepton{
+   
+     Employee e= new Employee("peter");
+   
+     Employee eClone = e.clone();
+    
+    eClone.setName("parker");
+  
+      System.out.println(e.getName());
+ 
+       System.out.println(eClone.getName());
+  
+  }
+}
+
+class Employee implements Cloneable{
+   
+ private String n;
+ 
+   public Employee(String name){
+     
+   this.n = name;
+ 
+   }
+ 
+   public String getName(String name){
+     
+   this.n=name;
+ 
+   }
+   
+ public Employee clone() throws CloneNotSupportException{
+  
+      return (Employee) super.clone();
+   
+ }
+}
