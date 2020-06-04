8a7ea56c9137de2937a7130da71340fea3b2d9b6
Gomathi-gif <61314863+Gomathi-gif@users.noreply.github.com>
2020-06-04 18:44:56 +0530

    Add files via upload

---
+++ set-day 6/collectiontree.java
@@ -0,0 +1,51 @@
+import java.util.Iterator;
+ 
+ import java.util.TreeSet;
+ 
+ public class Main{
+   
+   public Static void main(String[] args){
+  
+        TreeSet<String> set=new TreeSet<>();
+    
+      set.add("sita");
+    
+      set.add("Rita");
+  
+        set.add("Gita");
+    
+      set.add("Remo");
+      
+    Iterator<String> it = set.iterator();
+  
+        String query = "Gita";
+    
+      boolean result = false;
+    
+      while (it.hasNext()){
+           
+   if(it.next().equals(query)){
+          
+        result = true;
+              
+    break;
+           
+   }
+      } 
+ 
+     if
+      {
+  
+        (result System.out.println(query + "exists");
+  
+    }
+    
+  else 
+      {
+       
+   System.out.println(query +"doesn't exist";)
+  
+    }
+ 
+ }
+  }
---
+++ set-day 6/employeehash.java
@@ -0,0 +1,25 @@
+ import java.util.Iterator;
+  import java.util.HashSet;
+  
+public class Main{
+     
+ public Static void main(String[] args){
+      
+    HashSet<String> set=new HashSet<>();
+ 
+         set.add("sita");
+      
+    set.add("Rita");
+   
+       set.add("Gita");
+      
+    set.add("Remo");
+     
+     Iterator<String> it = set.iterator();
+   
+       while (it.hasNext()){
+  
+           System.out.println(it.next());
+ 
+     } 
+  }
